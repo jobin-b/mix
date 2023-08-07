8425640dc3b1fb375fa3df1a108bf932391cd383
jobin-b <jobinbebu.hpj@gmail.com>
2023-08-06 22:23:17 -0400

    spotify auth now works

---
+++ AxiosInstance.ts
@@ -0,0 +1,25 @@
+import Config from "react-native-config";
+import axios from "axios";
+const axiosInstance = axios.create({
+  baseURL: Config.BACKEND_URL,
+  timeout: 5000,
+}); // 5 seconds
+
+// axiosInstance.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem("token");
+
+//     // If the token exists in localStorage, add it to the request headers
+//     if (token) {
+//       config.headers["Authorization"] = `Bearer ${token}`;
+//     }
+
+//     return config;
+//   },
+//   (error) => {
+//     // Handle request error here
+//     return Promise.reject(error);
+//   }
+// );
+
+export default axiosInstance;
--- spotify/spotifyAuth.ts
+++ spotify/spotifyAuth.ts
@@ -1,60 +1,39 @@
 // https://developer.spotify.com/documentation/web-api/tutorials/code-pkce-flow
-import { createHash } from 'react-native-crypto';
-import { TextEncoder } from 'util';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import openLink from '../browser';
-// generate code verifier
-function generateRandomString(length: number): string {
-  let text = "";
-  let possible =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+import { authorize } from "react-native-app-auth";
 
-  for (let i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
-}
-
-// hashes it and creates the Code Challenge
-async function generateCodeChallenge(codeVerifier: string): Promise<string> {
-  function base64encode(string: ArrayBuffer): string {
-    return btoa(String.fromCharCode.apply(string))
-      .replace(/\+/g, "-")
-      .replace(/\//g, "_")
-      .replace(/=+$/, "");
-  }
-
-  const encoder = new TextEncoder();
-  const data = encoder.encode(codeVerifier);
-  const digest = await createHash("sha256").update(data).digest();
-
-  return base64encode(digest);
-}
-//
 
 const spotifyAuth = async () => {
-  const clientId = "YOUR_CLIENT_ID"; // TODO: Get this from backend
-  const redirectUri = "http://localhost:3000";
+  const config = {
+    
+    clientId: 'bded58f2b15c4916ab05f83fa770d6f8',
+    redirectUrl: 'myapp://auth-callback',
+    serviceConfiguration: {
+      authorizationEndpoint: 'https://accounts.spotify.com/authorize',
+      tokenEndpoint: 'https://accounts.spotify.com/api/token',
+    },
+    scopes: [
+      'user-read-playback-state',     // Get the User's Currently Playing Track
+      'user-modify-playback-state',   // Add An Item To The End Of User's Current Playback Queue
+      'user-read-currently-playing',  // Get the User's Queue
+      'streaming',                    // Play content and control playback on your other devices.
+      'user-read-private',            // Read access to user’s subscription details (type of user account).
 
-  let codeVerifier = generateRandomString(128);
+    ],
+    usePKCE: true,
+    skipCodeExchange: true,
+  };
 
-  const codeChallenge = await generateCodeChallenge(codeVerifier);
-  let state = generateRandomString(16);
-  let scope = ""; // TODO: prob env
+  try {
+    const res = await authorize(config);
+    const { codeVerifier, authorizationCode } = res;
 
-  await AsyncStorage.setItem('code_verifier', codeVerifier);
+  } catch(err){
+    console.log("spotify login error: ", err)
+  }
 
-  let args = new URLSearchParams({
-    response_type: "code",
-    client_id: clientId,
-    scope: scope,
-    redirect_uri: redirectUri,
-    state: state,
-    code_challenge_method: "S256",
-    code_challenge: codeChallenge,
-  });
+  
 
-  openLink('https://acciunts.spotify.com/authorize?' + args);
+
 };
 
 export default spotifyAuth;
